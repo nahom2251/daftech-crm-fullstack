519bc8136006e3fce3dd3e7f372bfe05e5fb522d
nahom2251 <nahomalehegne@gmail.com>
2026-08-04 11:52:43 +0300

    Update AppDbContextModelSnapshot.cs

--- api/src/DaftechCrm.Infrastructure/Migrations/AppDbContextModelSnapshot.cs
+++ api/src/DaftechCrm.Infrastructure/Migrations/AppDbContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace DaftechCrm.Infrastructure.Migrations
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
 
-            InitialCreateModel.Build(modelBuilder);
+            CurrentModel.Build(modelBuilder);
 #pragma warning restore 612, 618
         }
     }
